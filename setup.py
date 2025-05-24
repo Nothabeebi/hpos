2dedf7a83d3ada8ebd04009b113930ea4fffbfb8
Habeeb Abdullah <119491213+Nothabeebi@users.noreply.github.com>
2025-05-24 15:32:52 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1'
+
+setup(
+    name='hpos',
+    version=version,
+    description='High Performance POS for ERPNext',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
